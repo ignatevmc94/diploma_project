95bc32ed63e43bf35b114163190d5525630b1455
Michael Ignatev <Ignatevmc94@gmail.com>
2026-01-17 19:17:32 +0300

    add token obtain endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import ImportProductsView, ProductListView
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('import/', ImportProductsView.as_view()),
     path('products/', ProductListView.as_view()),
+    path('token/', obtain_auth_token,)
 ]
