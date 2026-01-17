03de9e071a670e3c69ffa104611d7728c4628b88
Michael Ignatev <Ignatevmc94@gmail.com>
2026-01-17 17:36:09 +0300

    add product list endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ImportProductsView
+from .views import ImportProductsView, ProductListView
 
 
 urlpatterns = [
     path('import/', ImportProductsView.as_view()),
+    path('products/', ProductListView.as_view()),
 ]
