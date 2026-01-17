cb4bc97f77d5f4982f6cf152f21d6b57e619bbe1
Michael Ignatev <Ignatevmc94@gmail.com>
2026-01-17 14:03:36 +0300

    add product models

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,62 @@
 from django.db import models
-
+from shops.models import Shop, Category
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category,
+        related_name='products', 
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+    
+
+class ProductInfo(models.Model):
+    product = models.ForeignKey(
+        Product, 
+        related_name='infos', 
+        on_delete=models.CASCADE
+    )
+    shop = models.ForeignKey(
+        Shop, 
+        related_name='products', 
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ('product', 'shop')
+
+
+class Parameter(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+class ProductParameter(models.Model):
+    product_info = models.ForeignKey(
+        ProductInfo, 
+        related_name='parameters', 
+        on_delete=models.CASCADE
+    )
+    parameter = models.ForeignKey(
+        Parameter, 
+        on_delete=models.CASCADE
+    )
+    value = models.CharField(max_length=255)
+
+
+
+
+
+
+
+
+
