764ec595732dd4be9c3249278ad9696291ae5c0b
Michael Ignatev <Ignatevmc94@gmail.com>
2026-01-17 19:40:20 +0300

    add cart endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ImportProductsView, ProductListView
+from .views import CartView, ImportProductsView, ProductListView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('import/', ImportProductsView.as_view()),
     path('products/', ProductListView.as_view()),
-    path('token/', obtain_auth_token,)
+    path('token/', obtain_auth_token),
+    path('cart/', CartView.as_view()),
 ]
