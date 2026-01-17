88b5f8794546871897fe52f32ba014224fd25dde
Michael Ignatev <Ignatevmc94@gmail.com>
2026-01-17 13:54:35 +0300

    add shop and category models

--- backend/shops/models.py
+++ backend/shops/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Shop(models.Model):
+    """
+    Модель магазина.
+    """
+    name = models.CharField(max_length=255)
+    url = models.URLField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """
+    Модель категории товаров.
+    """
+    name = models.CharField(max_length=255)
+    shops = models.ManyToManyField(Shop, related_name='categories')
+
+    def __str__(self):
+        return self.name
