e9cfe861b1ee270172e1faa415976cde7d5fbc90
Michael Ignatev <Ignatevmc94@gmail.com>
2026-01-25 21:35:09 +0300

    add Admin fields

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -6,8 +6,14 @@ from .models import Order, OrderItem
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
-    readonly_fields = ('product_info', 'total_price')
-    fields = ('product_info', 'quantity', 'total_price')
+    readonly_fields = ('product_info', 'total_price', 'get_shop')
+    fields = ('product_info', 'get_shop', 'quantity', 'total_price', 'status')
+
+    def get_shop(self, obj):
+        if obj.product_info and obj.product_info.shop:
+            return obj.product_info.shop.name
+        return "-"
+    get_shop.short_description = "Shop"
 
 
 @admin.register(Order)
--- backend/shops/admin.py
+++ backend/shops/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Shop, Category
+from .models import Shop
+from products.models import ProductInfo
 # Register your models here.
 
 
-admin.site.register(Shop)
-admin.site.register(Category)
+class ProductInfoInline(admin.TabularInline):
+    model = ProductInfo
+    extra = 0
+    fields = ("product", "price", "price_rrc", "quantity")
+    readonly_fields = ()
+    show_change_link = True
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "user", "is_accepting_orders")
+    inlines = [ProductInfoInline]
